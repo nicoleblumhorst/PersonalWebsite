578680d2ac98fb43d56cf4b52d5dc72eb88f9cb3
Nicole Blumhorst <nicoleblumhorst@gmail.com>
2015-12-01 19:18:21 -0600

    RESUME Set up jCarousel for the project section

--- src/js/resume.js
+++ src/js/resume.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
 
+    $('.age').text(calculateMyAge());
+
+    /* Skills highlight on mouseenter/mouseexit */
     $('.skill').on('mouseenter', function() {
-    	$(this).css("background", "rgba(255, 255, 255, 0.4)");
+        $(this).css("background", "rgba(255, 255, 255, 0.4)");
     });
 
     $('.skill').on('mouseleave', function() {
         $(this).css("background", "none");
     });
 
-    $('a[href*=#]').on('click', function(e)
+    /* Smooth scroll for navbar */
+    $('nav a[href*=#]').on('click', function(e)
     {
         e.preventDefault();
 
@@ -19,4 +23,68 @@ $(document).ready(function(){
         }
         return false;
     });
+
+    /* Project Carousel Configurations */
+    var projectWidth =$(window).width() * .6;
+    $('.project').width(projectWidth);
+    $('.jcarousel').width(projectWidth);
+    $('.jcarousel>ul').width(projectWidth * 2);
+
+    $('.jcarousel')
+        .jcarousel({
+            wrap: 'circular'
+        })
+        .jcarouselAutoscroll({
+            interval: 3000,
+            target: '+=1',
+            autostart: true
+        }) ;
+
+    $('.jcarousel-control-prev')
+        .on('jcarouselcontrol:active', function() {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function() {
+            $(this).addClass('inactive');
+        })
+        .jcarouselControl({
+            target: '-=1'
+        });
+
+    $('.jcarousel-control-next')
+        .on('jcarouselcontrol:active', function() {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function() {
+            $(this).addClass('inactive');
+        })
+        .jcarouselControl({
+            target: '+=1'
+        });
+
+    $('.jcarousel-pagination')
+        .on('jcarouselpagination:active', 'a', function() {
+            $(this).addClass('active');
+        })
+        .on('jcarouselpagination:inactive', 'a', function() {
+            $(this).removeClass('active');
+        })
+        .jcarouselPagination();
+
 });
+
+
+function calculateMyAge()
+{
+    var birthday = new Date(1991, 7, 2, null, null, null, null);
+    var today = new Date();
+
+    var age = today.getFullYear() - birthday.getFullYear();
+
+    var monthDif = today.getMonth() - birthday.getMonth();
+    if ( monthDif < 0 ||  (monthDif === 0 && (today.getDate() < birthday.getDate())) )
+    {
+        age--;
+    }
+    return age;
+}
